2719e3a4fc21270b4d12fbb37af4009c48585ea1
tiaha <tiahadoua@gmail.com>
2021-09-27 18:19:38 +0000

    Finalization of the connection logic

--- src/pages/examples/Signin.js
+++ src/pages/examples/Signin.js
@@ -1,45 +1,44 @@
 
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUnlockAlt,faHouseUser } from "@fortawesome/free-solid-svg-icons";
-import { Col, Row, Form, Button, Container, InputGroup } from '@themesberg/react-bootstrap';
-import axios from 'axios';
+import { faUnlockAlt, faHouseUser } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Form, Button, Spinner, Container, InputGroup } from '@themesberg/react-bootstrap';
+
 import { useCookies } from 'react-cookie';
 import { APPKEY, URLLOGIN } from "../constante/Const";
-
+import AxiosWebHelper from "../../utils/axios-helper";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Routes } from "../../routes";
 
 //Signin({ setToken })
-export default function Signin(){
+export default function Signin() {
 
   const [loginUsername, setLoginUsername] = useState('');
 
   const [loginPassword, setLoginPassword] = useState('');
-
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
   const [login, setLogin] = useState({});
 
+  const [cookies, setCookie, removeCookie] = useCookies(['token', 'id', 'user']);
 
- 
 
-  const [cookiesToken, setCookieToken] = useCookies(['token']);
-  const [cookiesId, setCookieId] = useCookies(['id']);
-  const [cookiesUser, setCookieUser] = useCookies(['user']);
-
-  
-
-  const handleOnUsernameChange = (event) =>{
-    console.log("username : " +event.target.value);
+  const handleOnUsernameChange = (event) => {
+    console.log("username : " + event.target.value);
     setLoginUsername(event.target.value);
   }
 
-  const handleOnPasswordChange = (event) =>{
-    
-    console.log("password : "+event.target.value);
+  const handleOnPasswordChange = (event) => {
+
+    console.log("password : " + event.target.value);
     setLoginPassword(event.target.value);
   }
 
 
+  const axios = AxiosWebHelper.getAxios();
+
   const loginHub2Support = (event) => {
-    
+
+    setIsLoginSuccess(false);
     console.log("will login");
     axios.post(
       URLLOGIN,
@@ -55,16 +54,26 @@ export default function Signin(){
     )
       .then((result) => {
         setLogin(result);
-        setCookieToken("token",result.data.token);
+        setCookie("token", result.data.token);
+        setCookie("id", result.data.id);
+        setCookie("user", result.data.user);
+        console.log("token in cookie login " + cookies.token)
+
+        console.log("id in cookie login " + cookies.id);
+
         console.log(result);
         console.log(result.data);
-        console.log(cookiesToken);
+
+
+        setIsLoginSuccess(true)
       })
-      .catch((error)=>{
+      .catch((error) => {
         if (error.response) {
           //console.log(error.response.data);
           console.log(error.response.status);
+          console.log(error.response.data);
           //console.log(error.response.headers);
+          setIsLoginSuccess(false);
         }
       })
   }
@@ -73,24 +82,24 @@ export default function Signin(){
     <main>
       <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
         <Container className="">
-         
+
           <Row className="justify-content-center form-bg-image " >
             <Col xs={12} className="d-flex align-items-center justify-content-center ">
               <div className="bg-white shadow-soft border rounded border-warning p-4 p-lg-5 w-100 fmxw-500">
                 <div className="text-center">
-                  <img className="mb-4 " src={require("../../assets/img/technologies/hub2.png")} width="100" height=""/>
+                  <img className="mb-2 " src={require("../../assets/img/technologies/hub2.png")} width="100" height="" />
                 </div>
                 <div className="text-center  text-md-center mb-4 mt-md-0">
                   <h3 className="mb-0">Support HUB2</h3>
                 </div>
                 <Form className="mt-4" >
-                  <Form.Group id="email" className="mb-4">
+                  <Form.Group id="email" className="mb-1">
                     <Form.Label></Form.Label>
                     <InputGroup className="border border-warning">
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faHouseUser} />
                       </InputGroup.Text>
-                      <Form.Control  required type="text" placeholder="Utilisateur" onChange={handleOnUsernameChange} />
+                      <Form.Control required type="text" placeholder="Utilisateur" onChange={handleOnUsernameChange} />
                     </InputGroup>
                   </Form.Group>
                   <Form.Group>
@@ -103,18 +112,22 @@ export default function Signin(){
                         <Form.Control required type="password" placeholder="Mot de passe" onChange={handleOnPasswordChange} />
                       </InputGroup>
                     </Form.Group>
-                    
+
                   </Form.Group>
-                 
-                  <Button type="button" className="w-100 btn-primary" onClick={loginHub2Support}>
+                  
+                  {isLoginSuccess ? <div className="d-flex justify-content-center">
+                    <Spinner animation="border " size="sm" role="status"></Spinner>
+                  </div> : <Button type="button" className="w-100 btn-primary" onClick={loginHub2Support}>
                     Connexion
-                  </Button>
+                  </Button> }
+
+                  {isLoginSuccess ? <Redirect to={Routes.Solde.path} /> : ""}
                 </Form>
                 <div className="d-flex justify-content-center my-4">
-                  
+
                 </div>
                 <div className="d-flex justify-content-center align-items-center mt-4">
-                  
+
                 </div>
               </div>
             </Col>

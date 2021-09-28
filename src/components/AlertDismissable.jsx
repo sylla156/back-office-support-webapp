9e01d4ea002a93ef9393ff8d91af24b9b00be56a
tiaha <tiahadoua@gmail.com>
2021-09-28 10:32:04 +0000

    Added alert dismissable function for loading and displaying error

---
+++ src/components/AlertDismissable.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Alert, Button, Spinner } from '@themesberg/react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
+
+function AlertDismissable({ message, variant, show, onClose, isLoading }) {
+  return (
+    <>
+      {isLoading
+        ? <div className="text-center">
+          <Spinner animation="border " size="sm" role="status">
+            <span className="visually-hidden">Chargement...</span>
+          </Spinner>
+        </div>
+        : <></>}
+      <Alert show={show} variant={variant} onClose={onClose}>
+        <div className="d-flex justify-content-between">
+          <div>
+            <FontAwesomeIcon icon={faBullhorn} className="mr-3" />
+            <span>{message}</span>
+          </div>
+          <Button variant="close" size="xs" onClick={() => onClose(variant)} />
+        </div>
+      </Alert>
+    </>
+  )
+}
+
+export default AlertDismissable
+

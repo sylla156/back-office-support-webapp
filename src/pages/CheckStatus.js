1aaff4a35f3c5a4000f336e760dcf0f728bcca64
tiaha <tiahadoua@gmail.com>
2021-10-14 13:18:50 +0000

    change of the name of the functions, suppression of the call to the api for the provider, addition of AlertDismissable, addition of the method to empty the list

--- src/pages/CheckStatus.js
+++ src/pages/CheckStatus.js
@@ -1,168 +1,150 @@
 import React, { useState } from "react";
 
-import { Col, Row, Form, Button, InputGroup } from '@themesberg/react-bootstrap';
+import { Col, Spinner, Row, Form, Button, InputGroup } from '@themesberg/react-bootstrap';
 import AxiosWebHelper from "../utils/axios-helper";
 
-
+import { APPKEY, BASEURLSOLDE } from "./constante/Const";
 import { Redirect } from 'react-router';
 import { Routes } from "../routes";
 import Hub2TransactionStatus from "../components/Hub2TransactionStatus";
 import ProviderTransactionStatus from "../components/ProviderTransactionStatus";
+import { withCookies, useCookies, Cookies } from 'react-cookie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import AlertDismissable from "../components/AlertDismissable";
 
 export default () => {
 
-  const [error, setError] = useState(null);
+  const [errorData, setErrorData] = useState(null);
   const [isLoaded, setIsLoaded] = useState(true);
   const [isLoadedProvider, setIsLoadedProvider] = useState(true);
   const [items, setItems] = useState([]);
   const [hub2Transfer, setHub2Transfer] = useState({});
-  const [idSupportHub, setIdSupportHub] = useState('');
+  const [transferIdSupport, setTransferIdSupport] = useState('');
 
   const [partnerIdTransactions, setPartnerIdTransactions] = useState('');
   const [providerTransfer, setProviderTransfer] = useState({});
   const [shouldLogin, setShouldLogin] = useState(false);
   const [selectOptionsProvider, setSelectOptionsProvider] = useState('');
 
+  const [cookies, setCookie] = useCookies(['token']);
 
   console.log("isLoaded before : " + isLoaded);
 
   const baseUrlSupportApiHub2 = "/transfers-status/hub2/";
 
-  
 
- 
+
+
 
   const axios = AxiosWebHelper.getAxios();
 
   const resquestHeaderSupportHub2 = {
     headers: {
-      "ApiKey": "b1413ec5-e76c-4c38-8a1f-38a7a67e0f7c",
-      "AuthenticationToken":"00cc25fc-bce5-48f2-a92f-4b14e1df386f"
+      AppKey: APPKEY,
+      authenticationtoken: cookies.token
     }
   };
 
-  console.log("id enter : " +idSupportHub);
+  console.log("id enter : " + transferIdSupport);
 
-  const checkHub2Status = function () {
+  const checkSupportTransferStatus = function () {
+    setErrorData(null)
     setIsLoaded(false);
-    axios.get(baseUrlSupportApiHub2 + idSupportHub, resquestHeaderSupportHub2)
+    console.log("===============> Before entered in checkSupportTransferStatus")
+    axios.get(baseUrlSupportApiHub2 + transferIdSupport, resquestHeaderSupportHub2)
       .then((result) => {
         setIsLoaded(true);
-        console.log("isLoaded in checkHub2Status: " + isLoaded);
-        setHub2Transfer(result.data);
+        console.log("============> isLoaded in checkSupportTransferStatus: " + isLoaded);
+        setHub2Transfer(result.data.hub2);
+        console.log("result.data.hub2");
+        console.log(result.data.hub2);
+
+        if (result.data.provider) {
+          console.log("result.data.provider define");
+          console.log("result.data.provider");
+          console.log(result.data.provider);
+          setProviderTransfer(result.data.provider);
+        }
+        else{
+          setProviderTransfer({});
+        }
+
+        console.log("result information ");
         console.log(result);
       })
-      .catch( error => {
+      .catch(error => {
         setIsLoaded(true);
-        setError(error);
-        if (error.response.status === 401) {
-          setShouldLogin(true);
-        }
-        else {
-          console.log(error.response.data.message);
+        if (error.response) {
+          console.log("In catch error checkSupportTransferStatus", error.response.data);
+          //console.log(error.response.data);
+          console.log("Status code error : " + error.response.status);
+          if (error.response.status === 401) {
+            console.log("===========> in error.response.status === 401 of checkSupportTransferStatus")
+            setShouldLogin(true);
+          }
+          else {
+            console.log("In atch error checkSupportTransferStatus")
+            console.log(error.response.data);
+            console.log(error.response.data.message);
+            setErrorData(error.response.data.message);
+          }
         }
       });
   }
 
-  const takeProviderByTheSelectOptionValue = (value)=>{
-
-    var baseUrlUrlProvider="";
-    var provider = value;
-    switch(provider){
-      case "Intouch":
-        return baseUrlUrlProvider = " /transfers-status/"+provider+"/ci/"+partnerIdTransactions;
-      
-      case "Orange":
-        return baseUrlUrlProvider = " /transfers-status/"+provider+"/ci/"+partnerIdTransactions;
-    }
+  const onFilters = ()=>{
+    setTransferIdSupport("");
+    setProviderTransfer({});
+    setHub2Transfer({});
   };
 
-  const resquestHeaderProvider = {
-    headers: {
-      "ApiKey": "b1413ec5-e76c-4c38-8a1f-38a7a67e0f7c",
-      "AuthenticationToken":"00cc25fc-bce5-48f2-a92f-4b14e1df386f"
-    }
-  };
 
-  console.log("Fournisseur api provider url by the takeProviderByTheSelectOptionValue : "+takeProviderByTheSelectOptionValue(selectOptionsProvider));
-
-  //setProviderTransfer
-  const checkProviderStatus = function () {
-    fetch(
-      //baseUrlUrlProvider,
-      takeProviderByTheSelectOptionValue(selectOptionsProvider),
-      resquestHeaderProvider,
-    )
-    .then(res => res.json())
-    .then((result)=>{
-      setProviderTransfer(result)
-      console.log(result)
-    },
-    (error) => {
-      setError(error);
-    })
-  }
-
-  
-  if(shouldLogin){
+  if (shouldLogin) {
     return <Redirect to={Routes.Signin.path} />;
   }
   return (
     <>
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
-
+    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4"></div>
+      <div >
+      <AlertDismissable  message={errorData} variant="danger" show={!!errorData} onClose={()=>setErrorData(null)}  />
+      <div>
+      
+    </div>
       </div>
 
       <div className="table-settings mb-4">
         <Row className="justify-content-between align-items-center">
 
-          <Col xs={8} md={6} lg={3} xl={6}>
+          <Col xs={12} md={6} lg={6} className="mb-2 px-2">
             <InputGroup>
               <InputGroup.Text>
 
               </InputGroup.Text>
-              <Form.Control type="text" placeholder="Transaction id" onChange={event => setIdSupportHub(event.target.value)} />
-            
-              <Button variant="outline-primary" size="sm" className="px-5" onClick={checkHub2Status}>Vérifier</Button>
-            </InputGroup>
-            
-          </Col>
+              <Form.Control type="text" placeholder="Transaction id" value={transferIdSupport} onChange={event => setTransferIdSupport(event.target.value)} />
 
-          <Col xs={8} md={2} lg={1} xl={6}>
-            <InputGroup>
-              <Form.Select aria-label="Default select example" defaultValue="Intouch" onChange={event => setSelectOptionsProvider(event.target.value)}>
-                  <option value="Intouch">Intouch CI</option>
-                  <option value="Orange">Orange CI</option>
-                  <option value="Mtn">Mtn CI</option>
-               </Form.Select> 
-              <InputGroup.Text>
-
-              </InputGroup.Text>
-              <Form.Control type="text" placeholder="Transaction Id" onChange={event => setPartnerIdTransactions(event.target.value)} />
-              <Button variant="outline-primary" type="submit" size="sm" className="px-5" onClick={checkProviderStatus}>Vérifier</Button>
+              {/* <Button variant="outline-primary" size="sm" className="px-5" onClick={checkHub2Status}>Vérifier</Button> */}
+              <Button variant="outline-primary" className="mx-2" type="button" onClick={onFilters}>Effacer</Button>
+              <Button className="ml-3" variant="primary" type="button" onClick={checkSupportTransferStatus} >Vérifier</Button>
             </InputGroup>
 
-
           </Col>
-
         </Row>
       </div>
 
-      <Row>
+      {isLoaded ? <Row>
         <Col xs={12} xl={6} >
-        {console.log("isLoaded in balise : " + isLoaded)}
-          {/* {isLoaded ? < TransferFormInformSourceHub transfer={hub2Transfer} /> : <Spinner animation="grow" sm/>} */}
-           
-          {console.log("hub 2 value id ")}
-          {console.log(hub2Transfer)}
-          <Hub2TransactionStatus hub2IdResult={hub2Transfer}/>
+          <Hub2TransactionStatus hub2IdResult={hub2Transfer}  hub2ErroData={errorData}/>
         </Col>
         <Col xs={12} xl={6} >
-          {console.log("isLoaded in balise : " + isLoadedProvider)}
-          {/* {isLoadedProvider ? <TransferFormInformSourceProvider intouch={providerTransfer} /> : <Spinner animation="grow" xl />} */}
-          <ProviderTransactionStatus provider={providerTransfer} />
+          <ProviderTransactionStatus providerTransfer={providerTransfer} />
         </Col>
-      </Row>
+      </Row> : <div className="d-flex justify-content-center">
+        <Spinner animation="border " size="sm" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>}
+
 
     </>
   );

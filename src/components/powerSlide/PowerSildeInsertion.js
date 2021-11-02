4ebd17432765177ecd87eaf2b9e9b2ac8505679c
tiaha <tiahadoua@gmail.com>
2021-11-02 15:01:04 +0000

    integrate power slide link

--- src/components/powerSlide/PowerSildeInsertion.js
+++ src/components/powerSlide/PowerSildeInsertion.js
@@ -58,8 +58,25 @@ export default function PowerSildeInsertion() {
     }
 
     return (
-        <div>
-            <iframe src="https://app.powerslide.io/slide/headless/fb0a604d-e6e8-44c4-86b8-b1d638239c30" width="100%" height="500" ></iframe>        
-        </div>
+        <>
+      <div>
+        <iframe
+          src="https://app.powerslide.io/slide/headless/d5b2762b-5094-4a4c-97df-23320fccdacc"
+          title="Weekly"
+          style={{ margin: "30px 0", display: " block;" }}
+          width="100%"
+          height="800"
+        ></iframe>
+      </div>
+      <div>
+        <iframe
+          src="https://app.powerslide.io/slide/headless/510bbce0-effc-411b-ab14-ab2fcfd3b8cc"
+          title="Transfers"
+          style={{ margin: "30px 0", display: "block" }}
+          width="100%"
+          height="800"
+        ></iframe>
+      </div>
+    </>
     )
 }

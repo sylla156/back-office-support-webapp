01f9b570ffcf07b5312ffb422a3ce4f5e7b784ac
tiaha <tiahadoua@gmail.com>
2021-11-04 11:50:58 +0000

    axios call deleted

--- src/components/powerSlide/PowerSildeInsertion.js
+++ src/components/powerSlide/PowerSildeInsertion.js
@@ -1,61 +1,7 @@
-import React, {useState, useEffect} from "react";
-import AxiosWebHelper from "../../utils/axios-helper";
-import {useCookies} from 'react-cookie';
-import { Redirect } from "react-router-dom";
-import {Routes} from "../../routes";
-import { POWERSLIDEFIRSTTRYNGAPIID, POWERSLIDEFIRSTTRYNGTOKEN, POWERSLIDEFIRSTTRYNGURL, POWERSLIDETRYINGACCESSSLIDEURL } from "../../pages/constante/Const";
+import React from "react";
+
 
 export default function PowerSildeInsertion() {
-
-    const [cookies] = useCookies(['token']);
-
-    const axios = AxiosWebHelper.getAxios();
-
-    const [isLoading, setIsLoading] = useState(true);
-
-    const [errorData, setErrorData] = useState('');
-
-    const [powerSlideData, setPowerSlideData] = useState([]);
-
-    const powerSlideAPi = ()=>{
-        console.log("in powerSlideAPi");
-        setIsLoading(true);
-        axios.post(
-            POWERSLIDEFIRSTTRYNGURL,
-            {
-                "username":"tiahadoua@gmail.com"
-            },
-            {
-                headers:{
-                    appId: POWERSLIDEFIRSTTRYNGAPIID,
-                    Authorization: `Bearer ${POWERSLIDEFIRSTTRYNGTOKEN}`
-                }
-            }
-        )
-        .then((result) =>{
-            console.log("in powerSlideAPi result ");
-            setIsLoading(false);
-            console.log(result);
-        })
-        .catch(error =>{
-            console.log("in powerSlideAPi catch error ");
-            console.log(error);
-        })
-
-    };
-
-
-
-
-    useEffect(() => {
-
-        powerSlideAPi();
-    
-    }, []);
-
-    if(!cookies.token){
-        <Redirect  to={Routes.Signin.path}/>
-    }
 
     return (
         <>
@@ -63,7 +9,7 @@ export default function PowerSildeInsertion() {
         <iframe
           src="https://app.powerslide.io/slide/headless/d5b2762b-5094-4a4c-97df-23320fccdacc"
           title="Weekly"
-          style={{ margin: "30px 0", display: " block;" }}
+          style={{ margin: "30px 0", display: "block" }}
           width="100%"
           height="800"
         ></iframe>

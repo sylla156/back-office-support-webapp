f75e003226b96e7ffa1bedfa8776e11d889340a9
tiaha <tiahadoua@gmail.com>
2021-11-05 16:44:57 +0000

    display a presentation by clicking on its button

--- src/components/powerSlide/PowerSildeInsertion.js
+++ src/components/powerSlide/PowerSildeInsertion.js
@@ -1,28 +1,45 @@
-import React from "react";
-
+import { Button, Col,InputGroup, Row } from "@themesberg/react-bootstrap";
+import React, {useState} from 'react';
+import { powerList } from "./powerSlideConst/powerConst";
 
 export default function PowerSildeInsertion() {
 
-    return (
-        <>
-      <div>
-        <iframe
-          src="https://app.powerslide.io/slide/headless/d5b2762b-5094-4a4c-97df-23320fccdacc"
-          title="Weekly"
-          style={{ margin: "30px 0", display: "block" }}
-          width="100%"
-          height="800"
-        ></iframe>
+  const [visibleTab, setVisibleTab] = useState(powerList[0].id);
+
+  return (
+    <>
+      <div className="table-settings mb-4">
+        <Row className="justify-content-between align-items-center">
+          <Col xs={12} md={6} lg={6} className="mb-2 px-2">
+            <InputGroup>
+              
+              {powerList.map((item)=>(
+                <Button variant="outline-primary" key={item.id} onClick={ ()=>{setVisibleTab(item.id)} } className="mx-2" active={visibleTab === item.id}  type="button">
+                {item.name}
+              </Button>
+              ))}
+            </InputGroup>
+          </Col>
+        </Row>
       </div>
-      <div>
-        <iframe
-          src="https://app.powerslide.io/slide/headless/510bbce0-effc-411b-ab14-ab2fcfd3b8cc"
-          title="Transfers"
-          style={{ margin: "30px 0", display: "block" }}
-          width="100%"
-          height="800"
-        ></iframe>
-      </div>
+      {
+       powerList.map((item)=>(
+         <>
+         {
+           visibleTab ===item.id && <Col>
+            <iframe
+              src={item.url}
+              title={item.title}
+              style={{ margin: "30px 0", display: "block" }}
+              width="100%"
+              height="800"
+            ></iframe>
+          </Col>
+         }
+         </>
+        ))
+      }
+      
     </>
-    )
+  );
 }

068f2452183a7ed0b5d8c3056d06c128baadd6ef
tiaha <tiahadoua@gmail.com>
2021-11-30 12:32:08 +0000

    merchant balance interface

---
+++ src/pages/MerchantBalance.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { Routes } from "../routes";
+import { useCookies } from "react-cookie";
+import AxiosWebHelper from "../utils/axios-helper";
+import { CounterWidgetMerchantBalance } from "../components/Widgets";
+import { APPKEY } from "./constante/Const";
+import { Col, Row, Spinner } from "@themesberg/react-bootstrap";
+
+export default () => {
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [merchantBalanceList, setMerchantBalanceList] = useState([]);
+  const [shouldLogin, setShouldLogin] = useState(false);
+
+  const [cookies] = useCookies(["token"]);
+
+  // const baseUrlSolde = "/balances";
+  const axios = AxiosWebHelper.getAxios();
+
+  const checkMerchantBalance = () => {
+    setIsLoaded(false);
+
+    console.log("isLoaded value : " + isLoaded);
+
+    console.log("authenticationtokend value : " + cookies.token);
+
+    console.log(" In check solde ");
+
+    axios
+      .get("", {
+        headers: {
+          AppKey: APPKEY,
+          authenticationtoken: cookies.token,
+        },
+      })
+      .then((result) => {
+        setIsLoaded(true);
+        console.log("isLoaded value : " + isLoaded);
+        setMerchantBalanceList(result.data);
+        console.log("In then");
+        console.log(result.data);
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        console.log("In the catch");
+        if (error.response) {
+          console.log("In catch error solde", error.response.data);
+          // console.log(error.response.data);
+          console.log("Status code error : " + error.response.status);
+          // console.log(error.response.headers);
+          if (error.response.status === 401) {
+            setShouldLogin(true);
+          } else {
+            console.log(error.response.data.message);
+          }
+        }
+      });
+  };
+
+  useEffect(() => {
+    //checkMerchantBalance();
+  }, []);
+
+  if (shouldLogin) {
+    return <Redirect to={Routes.Signin.path} />;
+  }
+
+  return (
+    <>
+      <Row>
+      <Col
+        xs={12}
+        sm={6}
+        md={5}
+        lg={4}
+        className="mb-4 border-warning "
+      >
+        <CounterWidgetMerchantBalance />
+      </Col>
+      <Col
+        xs={12}
+        sm={6}
+        md={5}
+        lg={4}
+        className="mb-4 border-warning "
+      >
+        <CounterWidgetMerchantBalance />
+      </Col>
+      <Col
+        xs={12}
+        sm={6}
+        md={5}
+        lg={4}
+        className="mb-4 border-warning "
+      >
+        <CounterWidgetMerchantBalance />
+      </Col>
+      </Row>
+    </>
+  );
+};

241d91fcf4822cc295d5774fcfa5ea824484548d
tiaha <tiahadoua@gmail.com>
2021-12-02 12:17:30 +0000

    changed merchant balance position in navba

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -127,8 +127,8 @@ export default (props = {}) => {
               
                                 {/* <NavItem title="Liste de transfert" icon={faExchangeAlt} link={Routes.TransferList.path} /> */}
                                 <NavItem title="Solde fournisseur" icon={faWallet} link={Routes.Solde.path} />
-                                <NavItem title="Vérification du statut" icon={faCheck} link={Routes.CheckStatus.path} />
                                 <NavItem title="Solde marchand" icon={faWallet} link={Routes.MechantBalance.path} />
+                                <NavItem title="Vérification du statut" icon={faCheck} link={Routes.CheckStatus.path} />
                                 {/* <NavItem title="Mise à jour du statut" icon={faSync} link={Routes.RefreshStatus.path}/> */}
                                 {/* <NavItem title="Solde fournisseur" icon={faWallet} link={Routes.Solde.path}/> */}
                             </CollapsableNavItem>

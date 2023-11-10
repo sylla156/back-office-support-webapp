6f661302f8d3b43bb94abf9fcfbfebd24729fccb
sylla <s67986601@gmail.com>
2023-11-10 10:15:34 +0000

    fix: i add the env var for update the axios helper file

--- src/utils/axios-helper.js
+++ src/utils/axios-helper.js
@@ -5,18 +5,10 @@ export default class AxiosWebHelper {
 
     static getAxios() {
 
-        // let baseURLToUse = 'http://' + window.location.hostname + ':4000/';
-        let baseURLToUse = "http://localhost" + ":3090/"; 
         const apiURL = process.env.REACT_APP_API_URL; // Defined at build time so don't forget to define the env var
-        // console.log(apiURL);
-        if (apiURL) {
-
-            baseURLToUse = apiURL;
-        
-        }
-
+       
         const instance = axios.create({
-            baseURL: baseURLToUse,
+            baseURL: apiURL,
             headers: {
                 'Content-Type': 'application/json',
                 'AppKey': APPKEY

4e4745f2ccb2ceff4cee4dc330a7762cb1a6b92e
Olivier Chevalier <olivier.chevalier@hub2.io>
2023-11-16 16:39:27 +0400

    fix(container): Updated perms on files & dirs

--- infra/docker/start_custom_nginx.sh
+++ infra/docker/start_custom_nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Setting up REACT_APP_API_URL to " $REACT_APP_API_URL
+echo "Setting up REACT_APP_API_URL."
 # Replace occurances of __REACT_APP_API_URL__ in builed js app by $REACT_APP_API_URL env variable
 find /usr/share/nginx/html -type f -name *.js -exec sed -i s+__REACT_APP_API_URL__+$REACT_APP_API_URL+g {} +
 
-echo "Setting up REACT_APP_APP_KEY to " $REACT_APP_APP_KEY
+echo "Setting up REACT_APP_APP_KEY."
 # Replace occurances of __REACT_APP_APP_KEY__ in builed js app by $REACT_APP_APP_KEY env variable
 find /usr/share/nginx/html -type f -name *.js -exec sed -i s+__REACT_APP_APP_KEY__+$REACT_APP_APP_KEY+g {} +
 

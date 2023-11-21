0d120ada9ae1bd40566be9e84d03f4c5e5813dd0
Olivier Chevalier <olivier.chevalier@hub2.io>
2023-11-21 14:19:09 +0400

    feat(FILE_LIMIT): Added env var to set max body size

--- infra/docker/start_custom_nginx.sh
+++ infra/docker/start_custom_nginx.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Setting up FILE_LIMIT."
+# Replace occurances of __REACT_APP_APP_KEY__ in builed js app by $REACT_APP_APP_KEY env variable
+sed -i "s/__HUB2_FILE_LIMIT__/$FILE_LIMIT/g" /etc/nginx/conf.d/default.conf
+
 echo "Setting up REACT_APP_API_URL."
 # Replace occurances of __REACT_APP_API_URL__ in builed js app by $REACT_APP_API_URL env variable
 find /usr/share/nginx/html -type f -name *.js -exec sed -i s+__REACT_APP_API_URL__+$REACT_APP_API_URL+g {} +

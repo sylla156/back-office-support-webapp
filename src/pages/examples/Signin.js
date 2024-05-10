9f0f5d533d3e4643c04ed775d9927a4d28d2cc87
Lionel Kitihoun <lionel.kitihoun@hub2.io>
2024-05-10 16:05:36 +0000

    fix: otp typo in SignIn and VerifyAuth components

--- src/pages/examples/Signin.js
+++ src/pages/examples/Signin.js
@@ -1,172 +1,127 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUnlockAlt, faHouseUser } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Form, Button, Spinner, Container, InputGroup } from '@themesberg/react-bootstrap';
 
-import React, {useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUnlockAlt, faHouseUser} from "@fortawesome/free-solid-svg-icons";
-import {Col, Row, Form, Button, Spinner, Container, InputGroup} from '@themesberg/react-bootstrap';
+import { useCookies } from 'react-cookie';
+import { APPKEY, URL_LOGIN } from '../constante/Const';
+import AxiosWebHelper from '../../utils/axios-helper';
+import { Redirect } from 'react-router-dom';
+import { Routes } from '../../routes';
+import AlertDismissable from '../../components/AlertDismissable';
 
-import {useCookies} from 'react-cookie';
-import {APPKEY, URL_LOGIN} from "../constante/Const";
-import AxiosWebHelper from "../../utils/axios-helper";
-import {Redirect} from "react-router-dom";
-import {Routes} from "../../routes";
-import AlertDismissable from "../../components/AlertDismissable";
-import VerifyAuth from "./VerifyAuth";
 
-// Signin({ setToken })
 export default function Signin() {
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+  const [shouldValidate2FA, setShouldValidate2FA] = useState(false);
+  const [otpAuthUrl, setOtpAuthUrl] = useState('');
+  const [, setLogin] = useState({});
+  const [error, setError] = useState(null);
+  const [cookies, setCookie] = useCookies(['token', 'id', 'user']);
+  const [isLoading, setIsLoading] = useState(false)
 
-    const [loginUsername, setLoginUsername] = useState('');
+  const handleOnUsernameChange = (event) => setLoginUsername(event.target.value);
+  const handleOnPasswordChange = (event) => setLoginPassword(event.target.value);
 
-    const [loginPassword, setLoginPassword] = useState('');
-    const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-    const [shouldValidate2FA, setShouldValidate2FA] = useState(false);
-    const [tOptUrl, setTOptUrl] = useState('');
-    const [, setLogin] = useState({});
-    const [error, setError] = useState(null);
-    const [cookies, setCookie] = useCookies(['token', 'id', 'user']);
-    const [isLoading, setIsLoading] = useState(false)
+  const axios = AxiosWebHelper.getAxios();
 
-    const handleOnUsernameChange = (event) => {
+  const loginHub2Support = () => {
+    const payload = { 'username': loginUsername, 'password': loginPassword };
+    const headers = { AppKey: APPKEY };
+    setError(null);
+    setIsLoading(true)
+    axios.post(URL_LOGIN, payload, headers)
+      .then((result) => {
+        setLogin(result);
+        setCookie('token', result.data.token);
+        setCookie('id', result.data.id);
+        setCookie('user', result.data.user);
+        setOtpAuthUrl(result.data.otpAuthUrl);
 
-        setLoginUsername(event.target.value);
-    
-    }
+        if (result.data.user.isActive2FA === false) {
+          setShouldValidate2FA(true);
+        } else {
+          setIsLoginSuccess(true);
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          setIsLoginSuccess(false);
+          setError(error.response.data.message);
+        } else if (error) {
+          setError(error.message);
+        } else {
+          setError('Une erreur est survenue.');
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-    const handleOnPasswordChange = (event) => {
+  const logo = require('../../assets/img/technologies/logo_o.png');
 
-        setLoginPassword(event.target.value);
-    
-    }
+  return (
+    <main>
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container className="">
 
+          <Row className="justify-content-center form-bg-image " >
+            <Col xs={12} className="d-flex align-items-center justify-content-center ">
+              <div className="bg-white shadow-soft border rounded border-warning p-4 p-lg-5 w-100 fmxw-500">
+                <div className="text-center">
+                  <img className="mb-2 " src={logo} width="100" height="" />
+                </div>
+                <div className="text-center  text-md-center mb-4 mt-md-0">
+                  <h3 className="mb-0">Support HUB2</h3>
+                </div>
+                <Form className="mt-4" >
+                  <Form.Group id="email" className="mb-1">
+                    <Form.Label></Form.Label>
+                    <InputGroup className="border border-warning">
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faHouseUser} />
+                      </InputGroup.Text>
+                      <Form.Control required type="text" placeholder="Utilisateur" onChange={handleOnUsernameChange} />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Group id="password" className="mb-4 ">
+                      <Form.Label></Form.Label>
+                      <InputGroup className="border border-warning">
+                        <InputGroup.Text >
+                          <FontAwesomeIcon icon={faUnlockAlt} />
+                        </InputGroup.Text>
+                        <Form.Control required type="password" placeholder="Mot de passe" onChange={handleOnPasswordChange} />
+                      </InputGroup>
+                    </Form.Group>
 
-    const axios = AxiosWebHelper.getAxios();
+                  </Form.Group>
 
-    const loginHub2Support = () => {
-
-        setError(null)
-        setIsLoading(true)
-        axios.post(
-            URL_LOGIN,
-            {
-                "username": loginUsername,
-                "password": loginPassword,
-            },
-            {
-                headers: {
-                    AppKey: APPKEY
-                }
-            }
-        )
-            .then((result) => {
-
-                setLogin(result);
-                setCookie("token", result.data.token);
-                setCookie("id", result.data.id);
-                setCookie("user", result.data.user);
-                setTOptUrl(result.data.tOtpAuthUrl)
-
-                if(result.data.user.isActive2FA === false){
-
-                    setShouldValidate2FA(true)
-                
-                }else{
-
-                    setIsLoginSuccess(true)
-                
-                }
-            
-            })
-            .catch((error) => {
-
-                if (error.response) {
-
-                    setIsLoginSuccess(false)
-                    setError(error.response.data.message)
-                
-                } else if (error) {
-
-                    setError(error.message)
-                
-                } else {
-
-                    setError('Une erreur est survenue.')
-                
-                }
-            
-            })
-            .finally(() => {
-
-                setIsLoading(false)
-            
-            })
-    
-    }
-
-    const logo = require('../../assets/img/technologies/logo_o.png')
-
-    return (
-        <main>
-            <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
-                <Container className="">
-
-                    <Row className="justify-content-center form-bg-image " >
-                        <Col xs={12} className="d-flex align-items-center justify-content-center ">
-                            <div className="bg-white shadow-soft border rounded border-warning p-4 p-lg-5 w-100 fmxw-500">
-                                <div className="text-center">
-                                    <img className="mb-2 " src={logo} width="100" height="" />
-                                </div>
-                                <div className="text-center  text-md-center mb-4 mt-md-0">
-                                    <h3 className="mb-0">Support HUB2</h3>
-                                </div>
-                                <Form className="mt-4" >
-                                    <Form.Group id="email" className="mb-1">
-                                        <Form.Label></Form.Label>
-                                        <InputGroup className="border border-warning">
-                                            <InputGroup.Text>
-                                                <FontAwesomeIcon icon={faHouseUser} />
-                                            </InputGroup.Text>
-                                            <Form.Control required type="text" placeholder="Utilisateur" onChange={handleOnUsernameChange} />
-                                        </InputGroup>
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Group id="password" className="mb-4 ">
-                                            <Form.Label></Form.Label>
-                                            <InputGroup className="border border-warning">
-                                                <InputGroup.Text >
-                                                    <FontAwesomeIcon icon={faUnlockAlt} />
-                                                </InputGroup.Text>
-                                                <Form.Control required type="password" placeholder="Mot de passe" onChange={handleOnPasswordChange} />
-                                            </InputGroup>
-                                        </Form.Group>
-
-                                    </Form.Group>
-                  
-                                    {isLoginSuccess ? <div className="d-flex justify-content-center">
-                                        <Spinner animation="border " size="sm" role="status"></Spinner>
-                                    </div> : <Button type="button" className="w-100 btn-primary" onClick={loginHub2Support}>
+                  {isLoginSuccess ? <div className="d-flex justify-content-center">
+                    <Spinner animation="border " size="sm" role="status"></Spinner>
+                  </div> : <Button type="button" className="w-100 btn-primary" onClick={loginHub2Support}>
                     Connexion
-                                    </Button> }
+                  </Button>}
 
-                                    <div className="mt-3">
-                                        <AlertDismissable message={error} variant="danger" show={!!error} onClose={() => setError(null)} isLoading={isLoading}/>
-                                    </div>
+                  <div className="mt-3">
+                    <AlertDismissable message={error} variant="danger" show={!!error} onClose={() => setError(null)} isLoading={isLoading} />
+                  </div>
 
-                                    {isLoginSuccess ? <Redirect to={Routes.VerifyAuth.path} /> : ""}
-                                    {shouldValidate2FA ? <Redirect to={{pathname:Routes.VerifyAuth.path, state:{tOtpAuthUrl: tOptUrl}}} /> : ""}
-                                </Form>
-                                <div className="d-flex justify-content-center my-4">
+                  {isLoginSuccess ? <Redirect to={Routes.VerifyAuth.path} /> : ""}
+                  {shouldValidate2FA ? <Redirect to={{ pathname: Routes.VerifyAuth.path, state: { otpAuthUrl } }} /> : ""}
+                </Form>
+                <div className="d-flex justify-content-center my-4">
 
-                                </div>
-                                <div className="d-flex justify-content-center align-items-center mt-4">
+                </div>
+                <div className="d-flex justify-content-center align-items-center mt-4">
 
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </main>
-    );
-
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
 }
-
